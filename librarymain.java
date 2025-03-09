c33003636382900e5e8c66bad229560d2af55f53
keshala123 <senuu08@gmail.com>
2025-03-09 11:34:40 +0530

    i created main java file for my library management system

---
+++ librarymain.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+// LibraryMain class
+public class LibraryMain {
+    public static void main(String[] args) {
+        Library library = new Library();
+        Scanner scanner = new Scanner(System.in);
+        char choice;
+
+ 
+
+        scanner.close();
+    }
+}
