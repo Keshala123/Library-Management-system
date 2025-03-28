4973ffc1d12633f018018a61459b6e625492f7e8
Keshala Senarath <168093603+Keshala123@users.noreply.github.com>
2025-03-28 08:41:22 +0530

    final file also added

---
+++ Book.java
@@ -0,0 +1,41 @@
+// Book class
+public class Book {
+    private String title;
+    private String author;
+    private String isbn;
+    private boolean isAvailable;
+
+    public Book(String title, String author, String isbn) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+        this.isAvailable = true;
+    }
+
+    // Getters and Setters
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+
+    public void setAvailable(boolean isAvailable) {
+        this.isAvailable = isAvailable;
+    }
+
+    @Override
+    public String toString() {
+        return "Title: " + title + ", Author: " + author + ", ISBN: " + isbn +
+               ", Available: " + (isAvailable ? "Yes" : "No");
+    }
+}
